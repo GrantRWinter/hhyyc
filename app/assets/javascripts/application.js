1ab6f2b36f05be512a87bb0aa605981cfa61def7
GrantRWinter <grantrwinter@gmail.com>
2014-06-14 19:46:59 -0600

    changed gems for styling and added sign up fields

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,11 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+
+
+
+
 //= require turbolinks
 //= require_tree .
